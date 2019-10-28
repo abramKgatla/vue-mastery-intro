6a06bc7c467c57c122a3df49c3d277663bb8c401
Abram Kgatla <mmla.kgatla@gmail.com>
2019-10-28 18:55:21 +0200

    computed properties lesson completed

--- main.js
+++ main.js
@@ -3,20 +3,21 @@ var app = new Vue({
     data: {
         brand: 'Vue Mastery',
         product: 'Socks',
-        image: './assets/vmSocks-green.jpg',
-        inStock: false,
+        selectedVaraint: 0,
         details: [
             "80% cotton", "20% polyester", "Gender-neutral"
         ],
         variants: [{
                 variantId: 2234,
                 variantColor: "Green",
-                variantImage: './assets/vmSocks-green.jpg'
+                variantImage: './assets/vmSocks-green.jpg',
+                variantQuantity: 10
             },
             {
                 variantId: 223,
                 variantColor: "Blue",
-                variantImage: "./assets/vmSocks-blue.jpg"
+                variantImage: "./assets/vmSocks-blue.jpg",
+                variantQuantity: 0
             },
         ],
         cart: 0,
@@ -25,13 +26,20 @@ var app = new Vue({
         addToCart: function() {
             this.cart += 1
         },
-        updateProduct: function(variantImage) {
-            this.image = variantImage
+        updateProduct: function(index) {
+            this.selectedVaraint = index
+            console.log(index)
         }
     },
     computed: {
         title: function() {
             return this.brand + ' ' + this.product
+        },
+        image() {
+            return this.variants[this.selectedVaraint].variantImage
+        },
+        inStock: function() {
+            return this.variants[this.selectedVaraint].variantQuantity
         }
     },
 });
