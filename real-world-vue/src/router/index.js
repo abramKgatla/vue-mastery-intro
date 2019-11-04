2b658d24ef926b4b1aa1ed6304aa368409e5b784
Abram Kgatla <mmla.kgatla@gmail.com>
2019-11-04 06:29:58 +0200

    lesson 5 completed

---
+++ real-world-vue/src/router/index.js
@@ -0,0 +1,34 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import EventList from '../views/EventList.vue'
+import EventCreate from '../views/EventCreate.vue'
+import EventShow from '../views/EventShow.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'event-list',
+    component: EventList
+  },
+  {
+    path: '/event/:id',
+    name: 'event-show',
+    component: EventShow,
+    props: true
+  },
+  {
+    path: '/event/create',
+    name: 'event-create',
+    component: EventCreate
+  }
+]
+
+const router = new VueRouter({
+  //removing the # in our url
+  mode: 'history',
+  routes
+})
+
+export default router
