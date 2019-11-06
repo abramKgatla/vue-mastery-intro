833519291fa2ba5fd4869e9a29e6ec1f564a69ea
Abram Kgatla <mmla.kgatla@gmail.com>
2019-11-06 18:25:59 +0200

    Before modularising axios

--- real-world-vue/src/components/EventCard.vue
+++ real-world-vue/src/components/EventCard.vue
@@ -1,30 +1,18 @@
 <template>
-  <router-link class="event-link" :to="{name: 'event-show', params: {id: '1'}}">
+  <router-link class="event-link" :to="{name: 'event-show', params: {id: event.id}}">
     <div class="event-card -shadow">
       <span class="eyebrow">@ {{event.time}} on {{event.date}}</span>
       <h4 class="title">{{ event.title }}</h4>
-      <BaseIcon name="users"></BaseIcon>
-      <span>{{event.attendees.length}} attending</span>
+      <BaseIcon name="users">{{event.attendees.length}} attending</BaseIcon>
     </div>
   </router-link>
 </template>
 
 <script>
   export default {
-    data() {
-      return {
-        event: {
-          id: 1,
-          title : 'Beach Cleanup',
-          date: 'Tuesday 05/10/2019',
-          time: '6:00pm',
-          attendees: [
-            {id: 'abc123', name: 'John okafor'},
-            {id: 'def456', name: 'Jabu Voroso'}
-          ]
-        }
-      }
-    },
+    props: {
+      event: Object
+    }
   }
 </script>
 
